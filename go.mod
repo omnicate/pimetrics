6c891f78be00a2178b8e58bd5f1c321aa490ed41
Holger Ihrig <holger@wgtwo.com>
2019-11-26 11:07:52 +0100

    Add SendCommand

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pimetrics
 
 go 1.13
 
-require github.com/prometheus/client_golang v1.2.1
+require (
+	github.com/prometheus/client_golang v1.2.1
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
--- pkg/pi-modem/pi-modem.go
+++ pkg/pi-modem/pi-modem.go
@@ -1,6 +1,35 @@
 package pi_modem
 
+import (
+	"errors"
+	"github.com/tarm/serial"
+	"log"
+)
+
 
 func Dummy() {
 	println("Some stuff")
+}
+
+
+func SendCommand(command string) (error, string) {
+	c := &serial.Config{Name: "/dev/serial0", Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		return errors.New("SendCommandInit: " + err.Error()), ""
+	}
+
+	n, err := s.Write([]byte(command))
+	if err != nil {
+		return errors.New("SendCommandWrite: " + err.Error()), ""
+	}
+
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		return errors.New("SendCommandRead: " + err.Error()), ""
+	}
+	log.Printf("%q", buf[:n])
+
+	return nil, string(buf[:n])
 }
