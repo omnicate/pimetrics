ec3bc482ef1e19e55e58b506c3c484e1c0ab0153
noy-wg2 <noy@wgtwo.com>
2019-11-26 11:55:17 +0100

    Fix JS code

--- pibuild.sh
+++ pibuild.sh
@@ -1,7 +1,8 @@
 TARGET=pimetrics_arm
 
-GOOS=linux GOARCH=arm go build -o ./bin/$TARGET
+env GOOS=linux GOARCH=arm go build -o ./bin/$TARGET
 if [ $? == 0 ]
 then
     scp ./bin/$TARGET wg2@192.168.1.110:/home/wg2/$TARGET
+    scp -r ./static wg2@192.168.1.110:/home/wg2/
 fi
