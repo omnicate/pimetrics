c3b748a039d2ea6c249913a38ce8563ce03d288d
Noy Shimshoni <noy@wgtwo.com>
2020-10-27 11:30:35 +0100

    update pibuild script

--- pibuild.sh
+++ pibuild.sh
@@ -1,8 +1,14 @@
 TARGET=pimetrics_arm
 
 env GOOS=linux GOARCH=arm go build -o ./bin/$TARGET
-if [ $? == 0 ]
+if [ $? == 0 ] || [ $1 == "pi3" ]
 then
     scp ./bin/$TARGET ubuntu@192.168.1.48:/home/ubuntu/$TARGET
+    scp ./config.yaml ubuntu@192.168.1.48:/home/ubuntu/config.yaml
     scp -r ./static ubuntu@192.168.1.48:/home/ubuntu/
+elif [ $1 == "pi4" ]
+then
+    scp ./bin/$TARGET ubuntu@192.168.1.108:/home/ubuntu/$TARGET
+    scp ./config.yaml ubuntu@192.168.1.108:/home/ubuntu/config.yaml
+    scp -r ./static ubuntu@192.168.1.108:/home/ubuntu/
 fi
